876ee8220a467921ca3f1dbee716b807548e6bff
John <john@pbsl>
2012-12-02 22:40:27 +0100

    Database changes for comments require upgrade.php to be run.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version    = 2012090600;
+$module->version    = 2012120200;
 $module->requires   = 2010112400;       // Moodle 2.0
 $module->component  = 'mod_slideshow';
 $module->cron       = 0;
