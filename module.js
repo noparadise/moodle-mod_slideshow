5727eb3bc77d0c6c043cc7ae8237d31e07008018
John <john@pbsl>
2012-12-28 07:10:02 +0100

    module.js: refactored. Updates mediaOutline size on blurring of the width and height text inputs.

--- module.js
+++ module.js
@@ -8,17 +8,15 @@ M.local_slideshow = {
     init : function(Y){
 			this.Y = Y;
 			this.slide = this.Y.one("#slide");
+			this.width = this.Y.one("#id_mediawidth");
+			this.height = this.Y.one("#id_mediaheight");
 
 			this.mediaOutline = this.Y.one("#media_outline");
 			this.mediaOutline.plug(this.Y.Plugin.Drag);
 			
 			// Set the outline box to the correct size and position.
-			var width = parseInt(this.Y.one("#id_mediawidth").get("value"), 10);
-			var height = parseInt(this.Y.one("#id_mediaheight").get("value"), 10);
-			if(!isNaN(width) && !isNaN(height)) {
-				this.mediaOutline.set("offsetWidth", width);
-				this.mediaOutline.set("offsetHeight", height);
-			}
+			this.updateMediaOutlineSize();
+			
 			var x = parseInt(this.Y.one("#id_mediaX").get("value"), 10);
 			var y = parseInt(this.Y.one("#id_mediaY").get("value"), 10);
 			if(!isNaN(x) && !isNaN(y)) {
@@ -27,6 +25,8 @@ M.local_slideshow = {
 			}
 
 			this.slide.on("click", this.updateMediaCoords);
+			this.Y.one("#id_mediawidth").on("blur", this.updateMediaOutlineSize);
+			this.Y.one("#id_mediaheight").on("blur", this.updateMediaOutlineSize);
     },
 
 		// Sets the media X and Y values taken from the draggable box's topleft X, Y coords.
@@ -38,5 +38,15 @@ M.local_slideshow = {
 			var relativeY = y - M.local_slideshow.slide.get("region").top;
 			M.local_slideshow.Y.one("#id_mediaX").set("value", relativeX);
 			M.local_slideshow.Y.one("#id_mediaY").set("value", relativeY);
+		},
+
+		updateMediaOutlineSize : function() {
+			var mediaOutline = M.local_slideshow.mediaOutline;
+			var width = parseInt(M.local_slideshow.Y.one("#id_mediawidth").get("value"), 10);
+			var height = parseInt(M.local_slideshow.Y.one("#id_mediaheight").get("value"), 10);
+			if(!isNaN(width) && !isNaN(height)) {
+				mediaOutline.set("offsetWidth", width);
+				mediaOutline.set("offsetHeight", height);
+			}
 		},
   }
