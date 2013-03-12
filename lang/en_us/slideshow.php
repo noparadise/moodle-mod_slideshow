492e97b77c4eaae9bc3e8d815aa3f2094088b9cd
Rex Lorenzo <rex@oid.ucla.edu>
2013-03-12 13:15:56 -0700

    Added en_us lang file and added module help string. Some minor wording/code cleanup.

---
+++ lang/en_us/slideshow.php
@@ -0,0 +1,4 @@
+<?PHP // $Id$ 
+
+$string['centred'] = 'Centered?';
+$string['keeporiginals'] = 'Keep originals?<br />If yes, the original image will be shown for printing. If no, only the resized version of the image is kept and server space is maximized.';
